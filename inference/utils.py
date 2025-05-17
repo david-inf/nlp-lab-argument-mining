cbb6cf713026ea69be64d07ffb3cab80162fb188
David Nardi <david.nardi1@hotmail.com>
2025-05-17 13:39:24 +0200

    improved inference loop

--- inference/utils.py
+++ inference/utils.py
@@ -1,9 +1,23 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import logging
+from rich.logging import RichHandler
 
 
-def plot_graph(vals_mat, ax, title):
+def get_logger():
+    """Logging mechanism"""
+    FORMAT = "%(message)s"
+    logging.basicConfig(
+        level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+    )
+    log = logging.getLogger("rich")
+    return log
+
+LOG = get_logger()
+
+
+def plot_graph(vals_mat, ax, title, xlab, ylab):
     # vals_max = [n_abstracts, AS, AR, 0/1]
     # Create scatter plot with two colors based on vals_mat[:, 2]
     excluded_idx = vals_mat[:, 2] == 0.
@@ -25,7 +39,10 @@ def plot_graph(vals_mat, ax, title):
     )
 
     ax.set_title(title)
-    ax.set_xlabel("argument ratio")
-    ax.set_ylabel("argument score")
+    ax.set_xlabel(xlab)
+    ax.set_ylabel(ylab)
     ax.legend()
     ax.grid(True)
+
+
+# def argscore_argratio()
